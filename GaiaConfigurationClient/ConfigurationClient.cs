bd6ec20c546be84a57efc132ecfe4ee02ae4a1a7
Vincent <rumble.vecent@outlook.com>
2021-07-02 10:47:24 +0800

    Add casted set ang get functions to make it more convenient to use.

--- GaiaConfigurationClient/ConfigurationClient.cs
+++ GaiaConfigurationClient/ConfigurationClient.cs
@@ -69,6 +69,66 @@ namespace Gaia.ConfigurationService
         }
 
         /// <summary>
+        /// Get the value of a integer configuration item.
+        /// </summary>
+        /// <param name="item_name">Name of the configuration item.</param>
+        /// <param name="default_value">Default value to return if given configuration item is empty.</param>
+        /// <returns>Value of the item or default value if the item is empty.</returns>
+        public int GetInteger(string item_name, int default_value)
+        {
+            var result = Database.StringGet($"configurations/{UnitName}/{item_name}");
+            return result.HasValue ? (int)result : default_value;
+        }
+        
+        /// <summary>
+        /// Get the value of a long integer configuration item.
+        /// </summary>
+        /// <param name="item_name">Name of the configuration item.</param>
+        /// <param name="default_value">Default value to return if given configuration item is empty.</param>
+        /// <returns>Value of the item or default value if the item is empty.</returns>
+        public long GetLong(string item_name, long default_value)
+        {
+            var result = Database.StringGet($"configurations/{UnitName}/{item_name}");
+            return result.HasValue ? (long)result : default_value;
+        }
+
+        /// <summary>
+        /// Get the value of a float configuration item.
+        /// </summary>
+        /// <param name="item_name">Name of the configuration item.</param>
+        /// <param name="default_value">Default value to return if given configuration item is empty.</param>
+        /// <returns>Value of the item or default value if the item is empty.</returns>
+        public double GetFloat(string item_name, double default_value)
+        {
+            var result = Database.StringGet($"configurations/{UnitName}/{item_name}");
+            return result.HasValue ? (double)result : default_value;
+        }
+        
+        /// <summary>
+        /// Get the value of a double configuration item.
+        /// </summary>
+        /// <param name="item_name">Name of the configuration item.</param>
+        /// <param name="default_value">Default value to return if given configuration item is empty.</param>
+        /// <returns>Value of the item or default value if the item is empty.</returns>
+        public double GetDouble(string item_name, double default_value)
+        {
+            var result = Database.StringGet($"configurations/{UnitName}/{item_name}");
+            return result.HasValue ? (double)result : default_value;
+        }
+
+        /// <summary>
+        /// Get the value of a boolean configuration item.
+        /// </summary>
+        /// <param name="item_name">Name of the configuration item.</param>
+        /// <param name="default_value">Default value to return if given configuration item is empty.</param>
+        /// <returns>Value of the item or default value if the item is empty.</returns>
+        public bool GetBoolean(string item_name, bool default_value)
+        {
+            var result = Database.StringGet($"configurations/{UnitName}/{item_name}");
+            return result.HasValue ? (bool)result : default_value;
+        }
+
+        /// <summary>
         /// Set the value of a configuration item.
         /// </summary>
         /// <param name="item_name">Name of the configuration item to set or add.</param>
@@ -76,6 +136,70 @@ namespace Gaia.ConfigurationService
         public void Set(string item_name, string item_value)
         {
             Database.StringSet($"configurations/{UnitName}/{item_name}", item_value);
+        }
+
+        /// <summary>
+        /// Set the value of a integer configuration item.
+        /// </summary>
+        /// <param name="item_name">Name of the configuration item to set or add.</param>
+        /// <param name="value">Value of the configuration item.</param>
+        public void Set(string item_name, int value)
+        {
+            Database.StringSet($"configurations/{UnitName}/{item_name}", value);
+        }
+        /// <summary>
+        /// Set the value of a unsigned integer configuration item.
+        /// </summary>
+        /// <param name="item_name">Name of the configuration item to set or add.</param>
+        /// <param name="value">Value of the configuration item.</param>
+        public void Set(string item_name, uint value)
+        {
+            Database.StringSet($"configurations/{UnitName}/{item_name}", value);
+        }
+        /// <summary>
+        /// Set the value of a long integer configuration item.
+        /// </summary>
+        /// <param name="item_name">Name of the configuration item to set or add.</param>
+        /// <param name="value">Value of the configuration item.</param>
+        public void Set(string item_name, long value)
+        {
+            Database.StringSet($"configurations/{UnitName}/{item_name}", value);
+        }
+        /// <summary>
+        /// Set the value of a unsigned long configuration item.
+        /// </summary>
+        /// <param name="item_name">Name of the configuration item to set or add.</param>
+        /// <param name="value">Value of the configuration item.</param>
+        public void Set(string item_name, ulong value)
+        {
+            Database.StringSet($"configurations/{UnitName}/{item_name}", value);
+        }
+        /// <summary>
+        /// Set the value of a float configuration item.
+        /// </summary>
+        /// <param name="item_name">Name of the configuration item to set or add.</param>
+        /// <param name="value">Value of the configuration item.</param>
+        public void Set(string item_name, float value)
+        {
+            Database.StringSet($"configurations/{UnitName}/{item_name}", value);
+        }
+        /// <summary>
+        /// Set the value of a double configuration item.
+        /// </summary>
+        /// <param name="item_name">Name of the configuration item to set or add.</param>
+        /// <param name="value">Value of the configuration item.</param>
+        public void Set(string item_name, double value)
+        {
+            Database.StringSet($"configurations/{UnitName}/{item_name}", value);
+        }
+        /// <summary>
+        /// Set the value of a boolean configuration item.
+        /// </summary>
+        /// <param name="item_name">Name of the configuration item to set or add.</param>
+        /// <param name="value">Value of the configuration item.</param>
+        public void Set(string item_name, bool value)
+        {
+            Database.StringSet($"configurations/{UnitName}/{item_name}", value);
         }
 
         /// <summary>
